17dc869dd0d5865a1cb5122e7829351758e943c9
Dotfile <>
2023-03-29 20:21:20 +0200

    fix name

--- backend/src/config/template-mapping.ts
+++ backend/src/config/template-mapping.ts
@@ -7,5 +7,24 @@ export const templateMapping = (
   individuals: IndividualInput[],
   metadata: CaseMetadata
 ): string => {
-  return process.env.TEMPLATE_ID;
+  let template_id = process.env.TEMPLATE_ID;
+
+  switch (metadata['company_structure']) {
+    case 'private_company':
+      template_id = process.env.TEMPLATE_PRIVATE_COMPANY;
+      break;
+    case 'public_company':
+      template_id = process.env.TEMPLATE_PUBLIC_COMPANY;
+      break;
+    case 'association':
+      template_id = process.env.TEMPLATE_ASSOCIATION;
+      break;
+    case 'self_employed':
+      template_id = process.env.TEMPLATE_SELF_EMPLOYED;
+      break;
+    default:
+      template_id = process.env.TEMPLATE_ID;
+  }
+
+  return template_id;
 };
