525f9a9ab9898a196663f27a20ea6398ecc53380
Dotfile <>
2023-03-29 23:22:50 +0200

    logo email

--- frontend/src/config/step.ts
+++ frontend/src/config/step.ts
@@ -13,6 +13,9 @@ export const stepsConfig: {
   pdfUrl?: string;
 }[] = [
   {
+    key: 'company_structure',
+  },
+  {
     key: 'company_search',
   },
   {
@@ -22,16 +25,10 @@ export const stepsConfig: {
     key: 'company_edit',
   },
   {
-    key: 'individuals_list',
-    hasApplicant: true,
+    key: 'additional_data',
   },
-  // Enable the individual_edit step only for a KYC
-  /*{
-    key: 'individual_edit',
-  },*/
-  // A step with a pdf viewer and a checkbox (like T&Cs) must contain "pdf_viewer" in the key (eg: pdf_viewer_terms)
   {
-    key: 'pdf_viewer_terms',
-    pdfUrl: 'https://acme.onboarding.dotfile.com/terms-and-conditions.pdf',
+    key: 'individuals_list',
+    hasApplicant: false,
   },
 ];
