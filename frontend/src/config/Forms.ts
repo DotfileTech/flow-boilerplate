86dc9f2306d9175a37ab1003d0cae3f23da92e30
Dotfile <>
2023-04-05 11:57:09 +0200

    additional questions

--- frontend/src/config/Forms.ts
+++ frontend/src/config/Forms.ts
@@ -1,11 +1,5 @@
 export const form = [
   {
-    key: 'activity_outside_eu',
-    type: 'radio',
-    after: 'company_edit',
-    choices: ['yes', 'no'],
-  },
-  {
     key: 'company_structure',
     type: 'radio',
     choices: [
@@ -15,31 +9,31 @@ export const form = [
       'self_employed',
     ],
   },
-  // {
-  //   key: 'additional_data',
-  //   type: 'form',
-  //   after: 'company_structure',
-  //   questions: [
-  //     {
-  //       id: 'activity',
-  //       required: true,
-  //       enabled: true,
-  //     },
-  //     {
-  //       id: 'turnover',
-  //       required: true,
-  //       enabled: true,
-  //     },
-  //     {
-  //       id: 'funds',
-  //       required: true,
-  //       enabled: true,
-  //     },
-  //     {
-  //       id: 'deposits',
-  //       required: true,
-  //       enabled: true,
-  //     },
-  //   ],
-  // },
+  {
+    key: 'additional_data',
+    type: 'form',
+    after: 'company_structure',
+    questions: [
+      {
+        id: 'activity',
+        required: true,
+        enabled: true,
+      },
+      {
+        id: 'turnover',
+        required: true,
+        enabled: true,
+      },
+      {
+        id: 'funds',
+        required: true,
+        enabled: true,
+      },
+      {
+        id: 'deposits',
+        required: true,
+        enabled: true,
+      },
+    ],
+  },
 ]
