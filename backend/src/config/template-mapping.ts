a0387ddd9a4a6b95db9fddbf5d085e5dc56e9af6
Thomas Bodin <thomas@dotfile.com>
2023-04-27 18:56:46 +0200

    🔧 Multi template + fix wording

--- backend/src/config/template-mapping.ts
+++ backend/src/config/template-mapping.ts
@@ -7,5 +7,30 @@ export const templateMapping = (
   individuals: IndividualInput[],
   metadata: CaseMetadata
 ): string => {
-  return process.env.TEMPLATE_ID;
+  let template_id = process.env.TEMPLATE_ID;
+
+  switch (metadata['origin_funds']) {
+    case 'professional_income':
+      template_id = process.env.TEMPLATE_PROFESSIONAL_INCOME;
+      break;
+    case 'holding_revenue':
+      template_id = process.env.TEMPLATE_HOLDING_REVENUE;
+      break;
+    case 'sale_of_securities':
+      template_id = process.env.TEMPLATE_SALE_OF_SECURITIES;
+      break;
+    case 'sales_of_property':
+      template_id = process.env.TEMPLATE_SALES_OF_PROPERTY;
+      break;
+    case 'inheritance_donation':
+      template_id = process.env.TEMPLATE_INHERITANCE_DONATION;
+      break;
+    case 'other_source':
+      template_id = process.env.TEMPLATE_OTHER_SOURCE;
+      break;
+    default:
+      template_id = process.env.TEMPLATE_ID;
+  }
+
+  return template_id;
 };
