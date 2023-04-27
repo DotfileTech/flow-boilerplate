61a23715a086e64450af22dfe656fa5b1197135b
Thomas Bodin <thomas@dotfile.com>
2023-04-27 10:42:04 +0000

    ✨ Email - Send email when a check is created (!9)

--- backend/src/controllers/webhooks.controller.ts
+++ backend/src/controllers/webhooks.controller.ts
@@ -15,7 +15,12 @@ class WebhooksController {
     try {
       const payload = req.body;
 
-      if (payload.event !== 'Check.Rejected') return res.status(200).json({});
+      if (
+        payload.event !== 'Check.Rejected' &&
+        payload.event !== 'Check.Started'
+      ) {
+        return res.status(200).json({});
+      }
 
       const caseId = payload.context.case.id;
       const check = payload.check;
@@ -30,6 +35,7 @@ class WebhooksController {
       );
 
       const { email, locale } = caseData.metadata;
+      const flags = caseData.flags;
 
       if (!email) {
         return res.status(200).json({});
@@ -41,6 +47,31 @@ class WebhooksController {
           : currentCheck.type;
       };
 
+      // Check.Started
+      if (
+        email &&
+        check.status === 'in_progress' &&
+        (flags.includes('for_review') ||
+          flags.includes('for_recollection') ||
+          flags.includes('all_checks_approved'))
+      ) {
+        await this.emailService.sendEmail(
+          email,
+          {
+            template: 'checkStarted',
+            subject: 'New document check',
+            message: '',
+          },
+          {
+            link: `${process.env.APP_URL}/?caseId=${caseId}`,
+            appLogoUrl: process.env.LOGO_URL,
+            checkTitle: exactType(check),
+          },
+          locale
+        );
+      }
+
+      // Check.Rejected
       switch (checkResult) {
         case 'rejected':
           if (email)
