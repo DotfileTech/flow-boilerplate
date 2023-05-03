64cc0c35129088a46a0a5fac27fe3213ce8b7b4e
Thomas Bodin <thomas@dotfile.com>
2023-05-03 17:00:35 +0200

    🔧 Add custom field: Trade registry location

--- frontend/src/config/Forms.ts
+++ frontend/src/config/Forms.ts
@@ -5,4 +5,14 @@ export const form: FormData[] = [
     key: 'disclaimer',
     fields: [],
   },
+  {
+    key: 'company_details',
+    after: 'company_edit',
+    fields: [
+      {
+        id: 'trade_registry_location',
+        isRequired: true,
+      },
+    ],
+  },
 ];
