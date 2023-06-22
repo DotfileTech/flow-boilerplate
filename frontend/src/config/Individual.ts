a4818dd4bdb09b134212e2e8ed1352a28bb5131e
mamizo <mamizo@dotfile.com>
2023-06-22 12:58:55 +0200

    🎉 init goldbroker kyc

--- frontend/src/config/Individual.ts
+++ frontend/src/config/Individual.ts
@@ -22,27 +22,14 @@ export const individualData: IndividualData[] = [
   {
     id: 'maiden_name',
     required: false,
-    enabled: true,
+    enabled: false,
   },
   {
-    // This field is mandatory to create an individual with the "applicant" role
-    id: 'email',
-    type: 'email',
-    required: false,
-    enabled: true,
-  },
-  {
-    id: 'phone_number',
-    type: 'tel',
-    required: false,
-    enabled: true,
-  },
-  {
-    // This field is mandatory to create an individual in a KYB
+    // This field is mandatory to create an individual
     id: 'roles',
     type: 'checkbox',
-    required: true,
-    enabled: true,
+    required: false,
+    enabled: false,
     options: [
       IndividualRoleEnum.beneficial_owner,
       IndividualRoleEnum.legal_representative,
@@ -50,30 +37,9 @@ export const individualData: IndividualData[] = [
     ],
   },
   {
-    id: 'ownership_percentage',
-    type: 'number',
-    required: false,
-    enabled: true,
-  },
-  {
-    id: 'position',
-    required: false,
-    enabled: true,
-  },
-  {
-    id: 'tax_identification_number',
-    required: false,
-    enabled: true,
-  },
-  {
-    id: 'social_security_number',
-    required: false,
-    enabled: true,
-  },
-  {
     id: 'birth_date',
     type: 'date',
-    required: false,
+    required: true,
     enabled: true,
   },
   {
@@ -88,9 +54,43 @@ export const individualData: IndividualData[] = [
     enabled: true,
   },
   {
+    id: 'ownership_percentage',
+    type: 'number',
+    required: false,
+    enabled: false,
+  },
+  {
+    id: 'position',
+    required: false,
+    enabled: false,
+  },
+  {
+    id: 'tax_identification_number',
+    required: false,
+    enabled: false,
+  },
+  {
+    id: 'social_security_number',
+    required: false,
+    enabled: false,
+  },
+  {
+    // This field is mandatory to create an individual with the "applicant" role
+    id: 'email',
+    type: 'email',
+    required: true,
+    enabled: true,
+  },
+  {
+    id: 'phone_number',
+    type: 'tel',
+    required: true,
+    enabled: true,
+  },
+  {
     id: 'street_address',
     nested: 'address',
-    required: false,
+    required: true,
     enabled: true,
   },
   {
@@ -102,32 +102,32 @@ export const individualData: IndividualData[] = [
   {
     id: 'postal_code',
     nested: 'address',
-    required: false,
+    required: true,
     enabled: true,
   },
   {
     id: 'city',
     nested: 'address',
-    required: false,
+    required: true,
     enabled: true,
   },
   {
     id: 'country',
     nested: 'address',
     type: 'country',
-    required: false,
+    required: true,
     enabled: true,
   },
   {
     id: 'iban',
     nested: 'banking_information',
-    required: false,
+    required: true,
     enabled: true,
   },
   {
     id: 'bic',
     nested: 'banking_information',
-    required: false,
+    required: true,
     enabled: true,
   },
 ];
--- frontend/src/config/step.ts
+++ frontend/src/config/step.ts
@@ -2,7 +2,7 @@ import { CustomField } from '../types';
 
 // @deprecated
 // To apply a specific design on the Checks list and card
-export const hasKyb = true;
+export const hasKyb = false;
 
 export const stepsConfig: {
   key: string;
@@ -13,25 +13,16 @@ export const stepsConfig: {
   pdfUrl?: string;
 }[] = [
   {
-    key: 'company_search',
-  },
-  {
-    key: 'company_list',
-  },
-  {
-    key: 'company_edit',
-  },
-  {
-    key: 'individuals_list',
-    hasApplicant: true,
-  },
-  // Enable the individual_edit step only for a KYC
-  /*{
     key: 'individual_edit',
-  },*/
-  // A step with a pdf viewer and a checkbox (like T&Cs) must contain "pdf_viewer" in the key (eg: pdf_viewer_terms)
+  },
   {
-    key: 'pdf_viewer_terms',
-    pdfUrl: 'https://acme.onboarding.dotfile.com/terms-and-conditions.pdf',
+    key: 'nationality',
+    fields: [
+      {
+        id: 'nationality1',
+        type: 'country',
+        isRequired: true,
+      },
+    ],
   },
 ];
