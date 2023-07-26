b8895401895ba17e5f53352f76b3156c3ff5973f
Thomas Bodin <thomas@dotfile.com>
2023-07-26 17:22:58 +0200

    🔧 Create template logic within Onboarding Flow
    resolves OPS-181

--- backend/src/config/template-mapping.ts
+++ backend/src/config/template-mapping.ts
@@ -7,42 +7,46 @@ export const templateMapping = (
   individuals: IndividualInput[],
   metadata: CaseMetadata
 ): string => {
-  if (
-    [
-      'AT',
-      'CZ',
-      'DE',
-      'IT',
-      'MT',
-      'RO',
-      'BE',
-      'DK',
-      'GR',
-      'LV',
-      'NL',
-      'SK',
-      'BG',
-      'EE',
-      'HU',
-      'LI',
-      'NO',
-      'SI',
-      'HR',
-      'FI',
-      'IS',
-      'LT',
-      'PL',
-      'ES',
-      'CY',
-      'FR',
-      'IE',
-      'LU',
-      'PT',
-      'SE',
-    ].includes(company.country)
-  ) {
-    return process.env.TEMPLATE_EEA;
+  if (metadata['Do you provide regulated services?'] === 'yes') {
+    if (
+      [
+        'AT',
+        'CZ',
+        'DE',
+        'IT',
+        'MT',
+        'RO',
+        'BE',
+        'DK',
+        'GR',
+        'LV',
+        'NL',
+        'SK',
+        'BG',
+        'EE',
+        'HU',
+        'LI',
+        'NO',
+        'SI',
+        'HR',
+        'FI',
+        'IS',
+        'LT',
+        'PL',
+        'ES',
+        'CY',
+        'FR',
+        'IE',
+        'LU',
+        'PT',
+        'SE',
+      ].includes(company.country)
+    ) {
+      return process.env.TEMPLATE_EEA;
+    } else {
+      return process.env.TEMPLATE_NO_EEA;
+    }
+  } else {
+    return process.env.TEMPLATE_ID;
   }
-
-  return process.env.TEMPLATE_ID;
 };
